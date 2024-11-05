fdc973abc7169809ffdb3cb5767656e95df92487
thiagogquinto <thiagogarianiq@gmail.com>
2024-11-05 13:27:53 -0300

    clear repository

---
+++ get_com_data.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import sys
+import os
+from pydriller import Git, Repository
+
+def get_commit_pr(commit_hash):
+    for commit in Repository("repos_dir/jabref", single=commit_hash).traverse_commits():
+        commit_author = commit.author.name
+        commiter = commit.committer.name 
+        commit_date = commit.author_date
+        committer_data = commit.committer_date
+        modified_files = [mod.filename for mod in commit.modified_files]
+        deletions = commit.deletions
+        insertions = commit.insertions
+        lines = commit.lines
+        dmm_unit_size = commit.dmm_unit_size
+        dmm_unit_complexity = commit.dmm_unit_complexity
+        dmm_unit_interfacing = commit.dmm_unit_interfacing
+
+        return commit_author, commiter, commit_date, committer_data, modified_files, deletions, insertions, lines, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing
+
+def process_file(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+        
+        for record in data:
+            commit_hash = record["commit_hash"]
+            commit_author, commiter, commit_date, committer_data, modified_files, deletions, insertions, lines, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing = get_commit_pr(commit_hash)
+            record["commit_author"] = commit_author
+            record["commiter"] = commiter
+            record["commit_date"] = commit_date
+            record["committer_data"] = committer_data
+            record["modified_files"] = modified_files
+            record["deletions"] = deletions
+            record["insertions"] = insertions
+            record["lines"] = lines
+            record["dmm_unit_size"] = dmm_unit_size
+            record["dmm_unit_complexity"] = dmm_unit_complexity
+            record["dmm_unit_interfacing"] = dmm_unit_interfacing
+
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=4)
+           
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python get_commit_pr.py <file_path>")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]
+    process_file(file_path)
