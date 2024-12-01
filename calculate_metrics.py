52513c0b3b0a00c7ac4cd8b2385b0ea9e6ad5f54
Thiago G. Q. <thiagogarianiq@gmail.com>
2024-12-01 16:24:16 -0300

    push

--- calculate_metrics.py
+++
@@ -1,4 +0,0 @@
-from utils.calculate_metrics import calculate_percentage_of_fixes_and_bics_with_test_changes, calculate_the_fix_commits_with_tests_that_become_bic
-
-calculate_percentage_of_fixes_and_bics_with_test_changes("./csv/commit_analizer.csv", "./csv/test_percentage.csv")
-calculate_the_fix_commits_with_tests_that_become_bic("./json/consolidated_data.json", "./csv/commit_analizer.csv", "./csv/test_interference.csv")
