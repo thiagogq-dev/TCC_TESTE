d06f4327c9ff38c15a3f1d73046a8a05169df59c
Thiago G. Q. <thiagogarianiq@gmail.com>
2025-01-07 21:19:00 -0300

    push

--- get_commit_data.py
+++ get_commit_data.py
@@ -10,15 +10,18 @@ def get_commit_data(commit_hash):
         commiter = commit.committer.name 
         commit_date = commit.author_date.isoformat()
         committer_data = commit.committer_date.isoformat()
-        modified_files = [mod.filename for mod in commit.modified_files]
+        nb_modified_files = len(commit.modified_files)
+        changed_files = commit.files
         deletions = commit.deletions
         insertions = commit.insertions
         lines = commit.lines
+        files_complexity = sum([file.complexity for file in commit.modified_files])
+        avg_files_complexity = files_complexity / nb_modified_files
         dmm_unit_size = commit.dmm_unit_size
         dmm_unit_complexity = commit.dmm_unit_complexity
         dmm_unit_interfacing = commit.dmm_unit_interfacing
 
-        return commit_author, commiter, commit_date, committer_data, modified_files, deletions, insertions, lines, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing
+        return commit_author, commiter, commit_date, committer_data, nb_modified_files, changed_files, deletions, insertions, lines, avg_files_complexity, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing
 
 def process_file(file_path):
     with open(file_path) as f:
@@ -26,15 +29,17 @@ def process_file(file_path):
         
         for record in data:
             commit_hash = record["fix_commit_hash"]
-            commit_author, commiter, commit_date, committer_data, modified_files, deletions, insertions, lines, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing = get_commit_data(commit_hash)
+            commit_author, commiter, commit_date, committer_data, modified_files, changed_files, deletions, insertions, lines, avg_files_complexity, dmm_unit_size, dmm_unit_complexity, dmm_unit_interfacing = get_commit_data(commit_hash)
             record["commit_author"] = commit_author
             record["commiter"] = commiter
             record["commit_date"] = commit_date
             record["committer_data"] = committer_data
-            record["modified_files"] = modified_files
+            record["nb_modified_files"] = modified_files
+            record["changed_files"] = changed_files
             record["deletions"] = deletions
             record["insertions"] = insertions
             record["lines"] = lines
+            record["avg_files_complexity"] = avg_files_complexity
             record["dmm_unit_size"] = dmm_unit_size
             record["dmm_unit_complexity"] = dmm_unit_complexity
             record["dmm_unit_interfacing"] = dmm_unit_interfacing
