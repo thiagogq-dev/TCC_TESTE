c437e55121bbd3d6edc3728dba7c0fc032a6104f
Thiago G. Q. <thiagogarianiq@gmail.com>
2025-01-24 23:02:34 -0300

    Fetch Sppring-Framework

--- split_files.py
+++ split_files.py
@@ -1,3 +1,3 @@
 from utils.utils import split_json_file
 
-split_json_file("./json/raw_data/final.json", "./json/raw_data/issues")
+split_json_file("./json/raw_data/issues_6.json", "./json/raw_data/issues_6")
