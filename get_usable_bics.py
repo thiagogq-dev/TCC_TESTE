2058aeee7162fcf5f15700ae561c088fd5806aa2
Thiago Gariani Quinto <thiagogarianiq@gmail.com>
2026-06-23 22:51:23 -0300

    add

--- get_usable_bics.py
+++ get_usable_bics.py
@@ -8,6 +8,7 @@ import re
 import sys
 
 OUTPUT_FOLDER = "data"
+ITERATIONS_FOLDER = "out"
 
 def build_known_results(out_folder: str) -> dict[str, dict]:
     """
@@ -77,7 +78,7 @@ def prepare_data(input_folder: str, first_actions_attempt: bool = False, known_r
                 candidate_bic = bics[-1] if bics else None
                 
                 if candidate_bic and known_results and candidate_bic in known_results:
-                    print(f"Commit {entry['fix_commit_hash']} em repo {entry['repo_name']} já processado, atualizando para não processar novamente.")
+                    print(f"Commit {entry['fix_commit_hash']} em repo {entry['repo_name']} já processado pelo SZZ, atualizando para não processar novamente.")
                     cached = known_results[candidate_bic]
                     already_processed_count.append({
                         **cached,
@@ -98,7 +99,7 @@ def prepare_data(input_folder: str, first_actions_attempt: bool = False, known_r
                     seen_commits.add(key)
                     results.append(result)
                 else:
-                    print(f"Commit {result['fix_commit_hash']} in repo {result['repo_name']} já processado, pulando.")
+                    print(f"Commit {result['fix_commit_hash']} em repo {result['repo_name']} armazenado para iteração futura, pulando.")
 
         with json_file.open("w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
@@ -110,9 +111,8 @@ def prepare_data(input_folder: str, first_actions_attempt: bool = False, known_r
     print(f"Total de commits já processados: {len(already_processed_count)}")
     return results
 
-def get_latest_version_folder(input_folder: Path) -> Path | None:
+def get_latest_version_folder(parent_folder: Path) -> Path | None:
     version_pattern = re.compile(r"^v(\d+)$")
-    parent_folder = input_folder.parent
 
     version_folders = []
     for child in parent_folder.iterdir():
@@ -136,19 +136,9 @@ def main() -> None:
     )   
 
     parser.add_argument(
-        "--already_processed_folder",
-        required=False,
-        help="Pasta contendo os arquivos de todas as versões anteriores do GitHub Actions, para verificar commits já processados",
-    )
-    parser.add_argument(
         "--next_version_folder",
         required=False,
-        help="A versão referente a próxima tentativa, para salvar os commits já processados",
-    )
-    parser.add_argument(
-        "--input_folder",
-        required=True,
-        help="Pasta contendo os arquivos de entrada JSON da última execução do GitHub Actions",
+        help="A versão referente a próxima tentativa, para salvar os commits já processados, se for None, será considerado que não há necessidade de salvar os commits já processados.",
     )
     parser.add_argument(
         "--file_prefix",
@@ -169,32 +159,14 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    input_folder = Path(args.input_folder)
     first_actions_attempt = args.first_actions_attempt
-    
-    if args.already_processed_folder:
-        if not os.path.exists(args.already_processed_folder):
-            confirmation = input(
-                f"Aviso: A pasta '{args.already_processed_folder}' não existe. "
-                "Deseja continuar mesmo assim? [y/N]: "
-                "Caso resposta 'y', o sript não verificará se há commits já processados e todos serão executados no pyszz. "
-            ).strip().lower()
-            if confirmation not in {"y", "yes", "s", "sim"}:
-                print("Execução cancelada pelo usuário.")
-                sys.exit(1)
-            else:
-                args.already_processed_folder = None
+    version_folder = args.next_version_folder
 
-    if not input_folder.exists():
-        raise FileNotFoundError(
-            f"Input folder '{input_folder}' does not exist."
-        )
-    
-    if not first_actions_attempt and "out/v" in str(input_folder):
+    latest_folder = get_latest_version_folder(ITERATIONS_FOLDER)
+    if latest_folder:
         print(
-            "Aviso: Parece que você está tentando processar arquivos de erro do GitHub Actions, mas o input_folder contém 'out/v', indicando que pode ser a "
-            "primeira tentativa. Considere usar a flag --first_actions_attempt "
-            "ou verificar se o input_folder está correto."
+            f"Aviso: o input_folder mais recente encontrado é '{latest_folder}'. "
+            "Por favor verifique se você deseja continuar com este diretório de entrada."
         )
         confirmation = input(
             "Deseja continuar mesmo assim? [y/N]: "
@@ -203,27 +175,13 @@ def main() -> None:
             print("Execução cancelada pelo usuário.")
             sys.exit(1)
 
-
-    latest_folder = get_latest_version_folder(input_folder)
-    if latest_folder and input_folder.name != latest_folder.name:
-        print(
-            f"Aviso: o input_folder informado foi '{input_folder}', mas o "
-            f"mais recente encontrado é '{latest_folder}'."
-        )
-        confirmation = input(
-            "Deseja continuar mesmo assim? [y/N]: "
-        ).strip().lower()
-        if confirmation not in {"y", "yes", "s", "sim"}:
-            print("Execução cancelada pelo usuário.")
-            sys.exit(1)
-
-    known_results = build_known_results(args.already_processed_folder) if args.already_processed_folder else None
+    known_results = build_known_results(ITERATIONS_FOLDER) if version_folder else None
 
     data = prepare_data(
-        input_folder=str(input_folder),
+        input_folder=str(latest_folder),
         first_actions_attempt=first_actions_attempt,
         known_results=known_results,
-        next_version_folder=args.next_version_folder
+        next_version_folder=version_folder
     )
 
     split_json_file(
